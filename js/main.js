939765a4740d8650c3d47ac795b10603f1f3e9b7
Reece-Dodgen <reecedodgen@gmail.com>
2018-08-06 16:02:52 +0200

    commit

---
+++ js/main.js
@@ -0,0 +1,44 @@
+
+
+    $(document).ready(function () {
+        $(window).scroll(function () {
+            if ($(this).scrollTop() > 100) {
+                $('#scroll').fadeIn();
+            } else {
+                $('#scroll').fadeOut();
+            }
+        });
+        $('#scroll').click(function () {
+            $("html, body").animate({ scrollTop: 0 }, 600);
+            return false;
+        });
+    });
+
+
+
+
+    $(function () {
+
+        var $window = $(window);		//Window object
+
+        var scrollTime = 1.2;			//Scroll time
+        var scrollDistance = 170;		//Distance. Use smaller value for shorter scroll and greater value for longer scroll
+
+        $window.on("mousewheel DOMMouseScroll", function (event) {
+
+            event.preventDefault();
+
+            var delta = event.originalEvent.wheelDelta / 120 || -event.originalEvent.detail / 3;
+            var scrollTop = $window.scrollTop();
+            var finalScroll = scrollTop - parseInt(delta * scrollDistance);
+
+            TweenMax.to($window, scrollTime, {
+                scrollTo: { y: finalScroll, autoKill: true },
+                ease: Power1.easeOut,	//For more easing functions see https://api.greensock.com/js/com/greensock/easing/package-detail.html
+                autoKill: true,
+                overwrite: 5
+            });
+
+        });
+
+    });
